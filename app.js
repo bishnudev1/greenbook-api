0169d0eeb7de57a74fbfd6fe64dafcafc411cc79
bishnudev1 <bishnudevkhutia20@gmail.com>
2023-04-27 00:42:47 +0530

    header-set

--- app.js
+++ app.js
@@ -8,25 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:3000'];
 
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.get('/', (req, res) => {
-    res.json({
-        data: "Server is working..."
-    });
+  res.json({
+    data: "Server is working..."
+  });
 })
 
 import userRoutes from './routes/userRoutes.js';
